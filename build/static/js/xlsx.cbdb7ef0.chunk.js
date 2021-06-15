043c087f03688912384f93e282bfd185d912a476
MuhammadRF <sam.fauzy@gmail.com>
2021-06-16 06:22:54 +0700

    delete build

--- build/static/js/xlsx.cbdb7ef0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpportal-ui"]=this["webpackJsonpportal-ui"]||[]).push([[2],{1202:function(n,i){},1276:function(n,i){},1277:function(n,i){}}]);
-//# sourceMappingURL=xlsx.cbdb7ef0.chunk.js.map
